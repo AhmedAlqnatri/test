8fdb6e7d023208be6627fe9ba734f727bc0e421b
Abdo-Saleh <micro.saleh@outlook.com>
2019-04-06 10:51:21 +0200

    by ahmed

---
+++ alqnatriahmed/src/classes/UserName.java
@@ -0,0 +1,20 @@
+package classes;
+
+public class UserName {
+
+	private String FirstName = null;
+	private String LastName = null;
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	
+}
---
+++ alqnatriahmed/src/classes/VisaRequest.java
@@ -0,0 +1,51 @@
+package classes;
+
+import java.util.Date;
+
+public class VisaRequest {
+
+	private long VisaId = 0;
+	private String UserName = null;
+	private UserName FullName = null;
+	private String ToCountry = null;
+	private Date ApplyDate = null;
+	private Boolean Status = false;
+	public long getVisaId() {
+		return VisaId;
+	}
+	public void setVisaId(long visaId) {
+		VisaId = visaId;
+	}
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	public UserName getFullName() {
+		return FullName;
+	}
+	public void setFullName(UserName fullName) {
+		FullName = fullName;
+	}
+	public String getToCountry() {
+		return ToCountry;
+	}
+	public void setToCountry(String toCountry) {
+		ToCountry = toCountry;
+	}
+	public Date getApplyDate() {
+		return ApplyDate;
+	}
+	public void setApplyDate(Date applyDate) {
+		ApplyDate = applyDate;
+	}
+	public Boolean getStatus() {
+		return Status;
+	}
+	public void setStatus(Boolean status) {
+		Status = status;
+	}
+	
+	
+}
